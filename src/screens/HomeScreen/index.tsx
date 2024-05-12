d0b699dce67fddd285921cc9ee19fce4d2f55b56
senaerbek <senaerbek@hotmail.com>
2024-05-12 19:38:30 +0300

    ADD header child component

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {HeaderComponent} from '../../components/HeaderComponent';
+import {styles} from './style.ts';
 
 export function HomeScreen() {
   return (
     <View>
       <HeaderComponent
         title="Adreslerim"
-        children={
-          <View>
-            <Text>Home Screen</Text>
-          </View>
-        }
+        children={<Text style={styles.moduleTitle}>Adres Bilgilerin</Text>}
       />
     </View>
   );
---
+++ src/screens/HomeScreen/style.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  moduleTitle: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#ffffff',
+  },
+});
