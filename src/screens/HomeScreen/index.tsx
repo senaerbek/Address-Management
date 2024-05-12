585e15108b3663262c0fa6f2309b10387f0ac8ba
senaerbek <senaerbek@hotmail.com>
2024-05-12 19:47:01 +0300

    ADD header component localization

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import {HeaderComponent} from '../../components/HeaderComponent';
 import {styles} from './style.ts';
+import {useLocalization} from '../../hooks/localization.ts';
 
 export function HomeScreen() {
+  const {t} = useLocalization();
+
   return (
     <View>
       <HeaderComponent
-        title="Adreslerim"
-        children={<Text style={styles.moduleTitle}>Adres Bilgilerin</Text>}
+        title={t('HOME_SCREEN.MY_ADDRESSES')}
+        children={
+          <Text style={styles.moduleTitle}>
+            {t('HOME_SCREEN.YOUR_ADDRESS_INFO')}
+          </Text>
+        }
       />
     </View>
   );
