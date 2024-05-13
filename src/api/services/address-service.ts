250493936e264948ea953f6ca8198eb211a8fb1d
senaerbek <senaerbek@hotmail.com>
2024-05-13 22:14:40 +0300

    ADD post address store

--- src/api/services/address-service.ts
+++ src/api/services/address-service.ts
@@ -1,10 +1,27 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {baseUrl} from '../index';
+import {Address} from '../../models/address.ts';
 
 export const getAddresses = createAsyncThunk(
   'addresses/fetchAddresses',
   async () => {
     const response = await fetch(`${baseUrl}/addresses`);
+    return await response.json();
+  },
+);
+
+export const addAddress = createAsyncThunk(
+  'addresses/addAddress',
+  async (address: Partial<Address>) => {
+    console.log(address);
+    const response = await fetch(`${baseUrl}/addresses`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(address),
+    });
+    console.log(response);
     return await response.json();
   },
 );
--- src/store/address/addressSlice.ts
+++ src/store/address/addressSlice.ts
@@ -1,6 +1,6 @@
 import {createSlice} from '@reduxjs/toolkit';
 import {Address} from '../../models/address.ts';
-import {getAddresses} from '../../api/services/address-service.ts';
+import {addAddress, getAddresses} from '../../api/services/address-service.ts';
 
 const addressSlice = createSlice({
   name: 'address',
@@ -19,6 +19,16 @@ const addressSlice = createSlice({
       state.loading = false;
     });
     builder.addCase(getAddresses.rejected, state => {
+      state.loading = false;
+    });
+    builder.addCase(addAddress.pending, state => {
+      state.loading = true;
+    });
+    builder.addCase(addAddress.fulfilled, (state, action) => {
+      state.addresses.push(action.payload);
+      state.loading = false;
+    });
+    builder.addCase(addAddress.rejected, state => {
       state.loading = false;
     });
   },
