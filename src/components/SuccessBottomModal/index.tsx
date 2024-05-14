e6dad7de0c406e57897ecb12d069052b7f8decf3
senaerbek <senaerbek@hotmail.com>
2024-05-14 23:31:42 +0300

    ADD SuccessBottomModal localization

--- src/components/SuccessBottomModal/index.tsx
+++ src/components/SuccessBottomModal/index.tsx
@@ -3,6 +3,7 @@ import {Text, View} from 'react-native';
 import React from 'react';
 import {styles} from './style.ts';
 import {SuccessIcon} from '../../assets/icons';
+import {useLocalization} from '../../hooks/localization.ts';
 
 interface SuccessBottomModalProps {
   isVisible: boolean;
@@ -11,6 +12,7 @@ interface SuccessBottomModalProps {
 
 export function SuccessBottomModal(props: SuccessBottomModalProps) {
   const {isVisible, setIsVisible} = props;
+  const {t} = useLocalization();
 
   return (
     <BottomModal isVisible={isVisible} setIsVisible={setIsVisible}>
@@ -18,7 +20,7 @@ export function SuccessBottomModal(props: SuccessBottomModalProps) {
         <View style={styles.successIconContainer}>
           <SuccessIcon style={styles.successIcon} />
         </View>
-        <Text style={styles.successText}>Adresin başarıyla kaydedildi!</Text>
+        <Text style={styles.successText}>{t('SUCCESS_MODAL.MESSAGE')}</Text>
       </View>
     </BottomModal>
   );
