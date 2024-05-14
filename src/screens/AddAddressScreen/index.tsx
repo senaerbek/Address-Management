0c0b2d4051441dcdce22d66c57436282c547ad99
senaerbek <senaerbek@hotmail.com>
2024-05-14 20:59:03 +0300

    ADD add address screen

--- src/screens/AddAddressScreen/index.tsx
+++ src/screens/AddAddressScreen/index.tsx
@@ -1,14 +1,38 @@
-import {Text, View} from 'react-native';
-import React from 'react';
-import {styles} from '../HomeScreen/style';
+import {ScrollView, Text, View} from 'react-native';
+import React, {useEffect, useMemo} from 'react';
+import {styles} from './style';
 import {HeaderComponent} from '../../components/HeaderComponent';
 import {useLocalization} from '../../hooks/localization';
+import {TextInputComponent} from '../../components/InputComponent';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppDispatch, RootState} from '../../store';
+import {DropdownComponent} from '../../components/DropdownComponent';
+import {getCities} from '../../api/services/city-service.ts';
+import {addAddress} from '../../api/services/address-service.ts';
+import {ButtonComponent} from '../../components/ButtonComponent';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export function AddAddressScreen() {
   const {t} = useLocalization();
+  const dispatch = useDispatch<AppDispatch>();
+  const {bottom} = useSafeAreaInsets();
+  const cities = useSelector((state: RootState) => state.cities.cities);
+
+  const citiesForDropdown = useMemo(() => {
+    return cities.map(city => {
+      return {
+        label: city.name,
+        value: city.name,
+      };
+    });
+  }, [cities]);
+
+  useEffect(() => {
+    dispatch(getCities());
+  }, []);
 
   return (
-    <View>
+    <View style={styles.container}>
       <HeaderComponent
         title={t('HOME_SCREEN.MY_ADDRESSES')}
         children={
@@ -16,6 +40,38 @@ export function AddAddressScreen() {
             {t('HOME_SCREEN.YOUR_ADDRESS_INFO')}
           </Text>
         }
+      />
+      <ScrollView style={styles.body} showsVerticalScrollIndicator={false}>
+        <TextInputComponent
+          style={styles.inputContainer}
+          label={t('ADD_ADDRESS_SCREEN.ADDRESS_INPUT')}
+        />
+        <TextInputComponent
+          style={styles.inputContainer}
+          label={t('ADD_ADDRESS_SCREEN.CITY_INPUT')}
+        />
+        <DropdownComponent
+          label={t('ADD_ADDRESS_SCREEN.DETAIL_INPUT')}
+          data={citiesForDropdown}
+          onChange={(value: any) => {
+            console.log(value);
+          }}
+          labelField={'label'}
+          valueField={'value'}
+        />
+      </ScrollView>
+      <View style={styles.divider} />
+      <ButtonComponent
+        style={[
+          styles.buttonContainer,
+          {
+            marginBottom: bottom,
+          },
+        ]}
+        title={t('ADD_ADDRESS_SCREEN.SAVE_BUTTON')}
+        onPress={() => {
+          //todo dispatch addAddress
+        }}
       />
     </View>
   );
---
+++ src/screens/AddAddressScreen/style.ts
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  moduleTitle: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#ffffff',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  body: {
+    margin: 20,
+  },
+  inputContainer: {
+    marginBottom: 12,
+  },
+  buttonContainer: {
+    marginHorizontal: 20,
+  },
+  divider: {
+    backgroundColor: '#EEF0F4',
+    height: 1,
+    marginVertical: 16,
+  },
+});
